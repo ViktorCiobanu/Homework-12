865226979a1f1c6f657dd3d197af3eed3d1f0bc6
ViktorCiobanu <ciobanu89@gmail.com>
2024-03-10 15:44:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const str = 'string'
+const num = 10
+const bool = true
+const und = undefined
+const nul = null
+const bigInt = BigInt(33)
+const symb = Symbol('')
+
+const obj = {}
+
+console.log (typeof str)
+
+const num1 = 2
+const num2 = 3
+
+console.log (num1 + num2)
+console.log (num1 - num2)
+console.log (num1 * num2)
+console.log (num1 / num2)
+console.log (num1 ** num2)
+console.log (num1 ++)
+console.log (num1 --)
+console.log (num1 += num2)
+console.log (num1 -= num2)
+
+console.log(num1 == num2)
+console.log(num1 === num2)
